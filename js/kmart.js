b40078f21753d1e28f1a4aa8f54b35d358db9cee
Dhilan <dhilan.patel87@gmail.com>
2017-07-02 01:08:11 +0530

    device detection added

--- js/kmart.js
+++ js/kmart.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
 		$(this).toggleClass('open');		
 		$('.menu-items').toggle('slide', { direction: "right" }, 200);
 	});
+	
+	$('.menu-items ul li a').click(function(event){
+		$('#menu-icon').toggleClass('open');
+		$('.menu-items').toggle('slide', { direction: "right" }, 200);
+
+		$('html, body').animate({
+			scrollTop: $( $.attr(this, 'href') ).offset().top
+		}, 1000);
+	})
 						  
 	new jQueryCollapse($(".feature-list ul li"), {
 		open: function() {
